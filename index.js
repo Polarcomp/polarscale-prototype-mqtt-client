bb025605c10bb2bae34644088e703c2d89ad616a
Caruy Chen <caruychen@gmail.com>
2023-02-16 10:36:47 +0200

    serve an html page

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 require('dotenv').config()
 const mqtt = require('mqtt');
+const express = require('express');
 const { InfluxDB, Point } = require('@influxdata/influxdb-client');
+const PORT = process.env.PORT || 3030;
+
+const app = express();
+
+// Environment Variables
 const url = process.env['INFLUX_URL'];
 const token = process.env['INFLUX_TOKEN'];
 const org = process.env['INFLUX_ORG'];
 const bucket = process.env['INFLUX_BUCKET'];
-const client = mqtt.connect('mqtt://broker.emqx.io');
 
+// Configcure clients
+const client = mqtt.connect('mqtt://broker.emqx.io');
 const writeApi = new InfluxDB({ url, token }).getWriteApi(org, bucket);
 
+/* MQTT CLient */
 client.subscribe('testuser/#');
 client.on('message', function (topic, payload) {
     const data = JSON.parse(payload.toString());
@@ -23,4 +31,11 @@ client.on('message', function (topic, payload) {
         console.log('Write finished');
     });
 });
-console.log('listening for MQTT messages');
+
+app.get('/', async(req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
