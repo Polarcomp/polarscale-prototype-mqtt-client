0922036d7d4405def8eb28ac40ae34440e6c93fd
Caruy Chen <caruychen@gmail.com>
2023-02-16 11:18:21 +0200

    remove http connection

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 require('dotenv').config()
 const mqtt = require('mqtt');
 const express = require('express');
-const path = require('path');
 const { InfluxDB, Point } = require('@influxdata/influxdb-client');
-const PORT = process.env.PORT || 3030;
-
-const app = express();
 
 // Environment Variables
 const url = process.env['INFLUX_URL'];
@@ -18,9 +14,10 @@ const client = mqtt.connect('mqtt://broker.emqx.io');
 const writeApi = new InfluxDB({ url, token }).getWriteApi(org, bucket);
 
 /* MQTT CLient */
-client.subscribe('testuser/#');
+client.subscribe('testuser1/#');
 client.on('message', function (topic, payload) {
     const data = JSON.parse(payload.toString());
+    console.log(topic, data);
     const point = new Point(data.point)
         .tag('device_type', data.device_type)
         .tag('device_id', data.device_id)
@@ -28,12 +25,4 @@ client.on('message', function (topic, payload) {
         .timestamp(data.timestamp)
         .floatField('weight', data.weight);
     writeApi.writePoint(point);
-});
-
-app.get('/', async(_, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
-});
-
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
 });
